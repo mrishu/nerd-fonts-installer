64a3e1393cad8c879f54ae4ef370d5d2a1c80727
mrishu <46226477+mrishu@users.noreply.github.com>
2023-06-18 06:49:13 +0530

    Add files via upload

--- nerdy.sh
+++ nerdy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 page=$(mktemp)
+download_selections=$(mktemp)
 
 # download and parse releases page of nerd fonts
 curl https://github.com/ryanoasis/nerd-fonts/releases > "$page"
@@ -13,43 +14,38 @@ starting_line="<table>"
 ending_line="<\/table><\/details><\/div>"
 sed -i -n '/^'"$starting_line"'$/,$p;/^'"$ending_line"'$/q' "$page"
 
-# select font that I want to download
-selection=$(sed -n 's_<td><a href="\([^"]*\)">\([^<]*\)</a></td>_\2\t\1_p' "$page" | column -t | fzf --reverse)
-[ -z "$selection" ] && exit
-filename=$(printf "$selection" | awk '{print $1}')
-download_link=$(printf "$selection" | awk '{print $2}')
+# select fonts that I want to download
+sed -n 's_<td><a href="\([^"]*\)">\([^<]*\)</a></td>_\2\t\1_p' "$page" | column -t | fzf --reverse --multi > "$download_selections"
 
-# make appropriate font directory in ~/.local/share/fonts/
-font_name=$(printf "$filename" | sed 's/\([^.]*\).*/\1/')
-font_directory="$HOME/.local/share/fonts/$font_name"
-if [ -d "$font_directory" ]; then
-	printf "$font_directory already exists!\
-	\nDo you wish to delete the existing directory and make a new one? [y/n] "
-	read -r ans
-	if [ "$ans" = 'y' ] || [ "$ans" = 'Y' ]; then
-		printf "Deleting directory $font_directory and placing new fonts in that directory.\n"
+while read -r selection ; do 
+	filename=$(printf "$selection" | awk '{print $1}')
+	download_link=$(printf "$selection" | awk '{print $2}')
+
+	# make appropriate font directory in ~/.local/share/fonts/
+	font_name=$(printf "$filename" | sed 's/\([^.]*\).*/\1/')
+	font_directory="$HOME/.local/share/fonts/$font_name Nerd Font"
+	if [ -d "$font_directory" ]; then
+		printf "\033[31mDirectory '$font_directory' already exists!\
+		\nDeleting directory '$font_directory' and making new '$font_directory'.\033[0m\n"
 		rm -rf "$font_directory"
-	else
-		printf "Aborting!\n"
-		exit
 	fi
-fi
-mkdir -p "$font_directory"
-cd "$font_directory"
+	mkdir -p "$font_directory"
+	cd "$font_directory" || exit 1
 
-# download font
-wget "$download_link"
+	# download font
+	wget "$download_link"
 
-# extract the downloaded file
-if printf "$filename" | grep -q '^.*\.tar\.xz$'; then
-	tar -xvf "$filename"
-elif printf "$filename" | grep -q '^.*\.zip$'; then
-	unzip "$filename"
-fi
+	# extract the downloaded file
+	if printf "$filename" | grep -q '^.*\.tar\.xz$'; then
+		tar -xvf "$filename"
+	elif printf "$filename" | grep -q '^.*\.zip$'; then
+		unzip "$filename"
+	fi
 
-# remove compressed file
-rm "$filename"
+	# remove compressed file
+	rm "$filename"
+done < "$download_selections"
+
 rm "$page"
-
 # refresh font cache
 fc-cache
